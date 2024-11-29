09ce4858b7d23c128dc73faf475c6633ab2a6eaa
YYT-t <yutongyin2028@u.northwestern.edu>
2024-11-28 22:04:13 -0600

    output

--- E_step_ent_test.py
+++ E_step_ent_test.py
@@ -319,8 +319,6 @@ class QTrainer(Trainer):
             # outputs = self.base_model(x, labels=x_labels, attention_mask=x_mask)
             # ce_loss_x, logits_x = outputs[:2]
             reward = - ce_loss.item() #- ce_loss_x.item()
-        print("reward:", reward)
-        print()
         log_Q = - model(xz, labels=xz_labels, attention_mask=xz_mask)[0]
         loss = -(reward - script_args.ent_coeff * log_Q.item()) * log_Q
         self.logger.info(f"loss:{loss}")

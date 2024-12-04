b4ce79c40985b0454f7252ef1f992ddf29253e05
ZH <64350615+YSLIU627@users.noreply.github.com>
2024-12-04 16:22:49 -0600

    fix bugs for not saving safetensors

--- baseline_sft_for_code/train.sh
+++ baseline_sft_for_code/train.sh
@@ -12,6 +12,6 @@ MODEL_NAME=deepseek-ai/deepseek-coder-6.7b-instruct
 MODEL_SAVE_NAME=baseline-deepseek-coder-6.7b-instruct-sft
 DATASET_NAME=OpenCoder-LLM/opc-sft-stage2
 
-ACCELERATE_LOG_LEVEL=info accelerate launch --main_process_port  $PORT1 --num_processes 8 baseline_sft.py --deepspeed deepspeed_configs/deepspeed_3.json  --model_name ${MODEL_NAME} --output_dir ./${MODEL_SAVE_NAME} --hub_model_id ${MODEL_SAVE_NAME} --learning_rate 5e-5 --num_train_epochs 1 --Task_Type ${TASK_ID} --per_device_train_batch_size 16 --gradient_accumulation_steps 4 --wandb_project EM
+ACCELERATE_LOG_LEVEL=info accelerate launch --main_process_port  $PORT1 --num_processes 8 baseline_sft.py --deepspeed deepspeed_configs/deepspeed_2.json  --model_name ${MODEL_NAME} --output_dir ./${MODEL_SAVE_NAME} --hub_model_id ${MODEL_SAVE_NAME} --learning_rate 5e-5 --num_train_epochs 1 --Task_Type ${TASK_ID} --per_device_train_batch_size 4 --gradient_accumulation_steps 16 --wandb_project EM
 
 #bash eval.sh ./${MODEL_SAVE_NAME}

25623864d4ae6517730ec414cae25537ca7e23af
xiaojun.xu <xiaojun.xu@bytedance.com>
2024-12-22 09:01:31 +0800

    eval script

--- eval.sh
+++ eval.sh
@@ -7,10 +7,13 @@
 huggingface-cli login --token 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
 
 
-lm_eval --model hf \
-    --model_args pretrained=YYT-t/gemma-2-9b-it_gsm8k_ent0.05_beam5_dosampleFalse_temp0.8_estep__final\
+    #--model_args pretrained=./xiaojun_out/PPO_ansprob/final_checkpoint\
+    #--model_args pretrained=YYT-t/PPO_ansprob_9b_final_checkpoint\
+mlx worker launch --gpu=1 --type=a100-80g -- lm_eval --model hf \
+    --model_args pretrained=google/gemma-2-9b-it\
+	--apply_chat_template \
     --tasks gsm8k \
-    --device cuda:4 \
+    --device cuda:0 \
     --batch_size 8 \
     --output_path ./Logs \
     --log_samples \
